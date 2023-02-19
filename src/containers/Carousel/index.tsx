6fae1abcb632fb972d01e804517ea13d5828728a
Monaim Touinssi <mtouinssi@miral.ae>
2023-02-19 20:58:23 +0100

    remove comment

--- src/containers/Carousel/index.tsx
+++ src/containers/Carousel/index.tsx
@@ -30,28 +30,6 @@ const index = () => {
               ></button>
             )
           )}
-          {/* <button
-            type="button"
-            data-bs-target="#header-carousel"
-            data-bs-slide-to="0"
-            className=""
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#header-carousel"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-            className=""
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#header-carousel"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-            className="active"
-            aria-current="true"
-          ></button> */}
         </div>
         <div className="carousel-inner">
           {siteData?.pages?.home?.containers?.carousel?.items.map(
